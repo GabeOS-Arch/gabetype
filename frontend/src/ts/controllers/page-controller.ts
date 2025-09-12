monkeytypegame/monkeytype

feat: add friend requests frontend (@fehmer)

frontend/src/ts/controllers/page-controller.ts
@@ -12,6 +12,7 @@ import * as PageProfileSearch from "../pages/profile-search";
 import * as Page404 from "../pages/404";
 import * as PageLeaderboards from "../pages/leaderboards";
 import * as PageAccountSettings from "../pages/account-settings";
+import * as PageFriends from "../pages/friends";
 import * as PageTransition from "../states/page-transition";
 import * as AdController from "../controllers/ad-controller";
 import * as Focus from "../test/focus";
@@ -78,6 +79,7 @@ export async function change(
       404: Page404.page,
       accountSettings: PageAccountSettings.page,
       leaderboards: PageLeaderboards.page,
+      friends: PageFriends.page,
     };
 
     const previousPage = pages[ActivePage.get()];

GabeOS-Arch/gabetype

frontend/src/ts/controllers/page-controller.ts
@@ -9,6 +9,7 @@ import * as PageLogin from "../pages/login";
 import * as PageLoading from "../pages/loading";
 import * as PageProfile from "../pages/profile";
 import * as PageProfileSearch from "../pages/profile-search";
+import * as Friends from "../pages/friends";
 import * as Page404 from "../pages/404";
 import * as PageLeaderboards from "../pages/leaderboards";
 import * as PageAccountSettings from "../pages/account-settings";
@@ -186,6 +187,7 @@ export async function change(
     login: PageLogin.page,
     profile: PageProfile.page,
     profileSearch: PageProfileSearch.page,
+    friends: Friends.page,
     404: Page404.page,
     accountSettings: PageAccountSettings.page,
     leaderboards: PageLeaderboards.page,